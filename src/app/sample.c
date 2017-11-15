dc8fc6172eaa759918295619a37a26fc06de21e6
lxp1413801 <lxp1413801@gmail.com>
2017-11-15 16:45:00 +0800

    dev

--- src/app/sample.c
+++ src/app/sample.c
@@ -1,11 +1,38 @@
 #include "../includes/includes.h"
+volatile uint8_t sampleIndex=0x00;
+
+void samlpe_chip0_channle_diff_press(void)
+{
+	
+}
+void samlpe_chip0_channle_press(void)
+{
+	
+}
+void samlpe_chip0_channle_temperature(void)
+{
+	
+}
+
+void sample_fast_mode(void)
+{
+	uint8_t t8;
+	t8=sampleIndex;
+	switch(t8){
+		case 0x00:samlpe_chip0_channle_diff_press();break;
+		case 0x01:samlpe_chip0_channle_press();break;
+		case 0x03:samlpe_chip0_channle_temperature;break;
+		default:break;
+	}
+}
+
 void thread_sample( void * pvParameters )
 {
+	
 	while(1){
 		osDelay(100);
 		__nop();
 		__nop();
-		
 	}
 }
 void thread_sample_void(void)
